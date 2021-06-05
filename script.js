844119ef353b0235cfdda9bc22b49c129b488334
osamaalbasha <>
2021-06-05 19:26:58 +0000

    add js

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Welcome");
+
+
+
+
+ var user = prompt("enter your name");
+var number = prompt("How many people would like to travel with us? ");
+
+if("number >=10"){alert("Congratulations you will get a discount")}else{alert("I hope you find what you want on our page")};
+
+document.write("<p style='color:green;'>The work of the student Osama Al-Basha </p>")
+
+
+
+
