d8d93ad1bc73faf7bfd64e620b902d22ec27a29f
osamaalbasha <>
2021-06-06 19:50:46 +0000

    add loop

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+var favtrip = prompt("whats your fav from :Aqaba, Dubai, Istanbul");
+
+while (favtrip != "Aqaba" && favtrip !="Dubai" && favtrip !="Istanbul" ) {
+
+    favtrip = prompt("please enter on of : Aqaba, Dubai, Istanbul");
+  }
+
+
+
+
 alert("Welcome");
+
 
 
 
@@ -6,12 +17,21 @@ alert("Welcome");
  var user = prompt("enter your name");
 var number = prompt("How many people would like to travel with us? ");
 
-var xr =prompt ("what you like color ?")
-if("number =>10"){alert("Congratulations you will get a discount")}else{alert("I hope you find what you want on our page")};
+var xr =prompt ("what you like color ?");
+if("number >=10"){alert("Congratulations you will get a discount")}else{alert("I hope you find what you want on our page")};
+
+
+document.write("<p style='color:" + xr + ";'>" + user + "</p>");
+
+var nn= prompt("How many icon planes do you want to see on the page?")
+
+for(var i=0 ;i<nn;i++){
+document.write("<img src='https://cdn.iconscout.com/icon/free/png-256/plane-2359613-1987480.png'/>")}
 
 
 
-document.write("<p style='color:" + xr + ";'>" + user + "</p>")
+
+
 
 
 
